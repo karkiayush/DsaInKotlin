989c3d36a0912a4296fd8c63934bd2f785c83bd7
karkiayush <karkiayush619@gmail.com>
2024-01-16 22:28:15 +0545

    some complexity concept is explored

---
+++ src/main/kotlin/TimeComplexity/ComplexityInfo.kt
@@ -0,0 +1,23 @@
+package TimeComplexity
+
+/**
+ *How well does it scale?
+ *ans: The term scalability is one of the important terms in domain of technology and computer programming. The term
+ * scalability is used with various prefix term like in context of software architecture, database, server etc.
+ *
+ * For instance: The scalability of database refers, how much client can it handler it easily without facing any sort
+ * of problem.
+ *
+ * In the same way, complexity in terms of algorithm denotes the performance of the algorithm in execution time and
+ * memory space consumption when the input size increases. With small amounts of data, even the most expensive
+ * algorithm can seem fast due to the speed of modern hardware. However, as data increases, the cost of an expensive
+ * algorithm becomes increasingly apparent.bigger, and algo becomes crippling.
+ *
+ *
+ *
+ * <==================Time Complexity===================>
+ *Time complexity is the measurement of time required to run an algorithm as the input size increases.
+ *
+ * There are different types of time complexities like constant, linear, quadratic, logarithmic etc which are
+ * explained in their individual codes.
+ * */
---
+++ src/main/kotlin/TimeComplexity/ConstantTimeComplexity.kt
@@ -0,0 +1,21 @@
+package TimeComplexity
+
+/**A constant time algo is the one that have the same running time regardless of its input size.*/
+
+fun main() {
+    checkFirst()
+}
+
+fun checkFirst() {
+    /** Here you can see that the size of the name doesn't effect the running time of the algorithm since it only
+     * checks the first element of the list.
+     *
+     * So the time complexity is O(1)*/
+    val nameList = arrayListOf("Warner", "Steven", "David")
+
+    if (nameList.firstOrNull() == null) {
+        println("Empty name list")
+    } else {
+        println("Name list is not empty")
+    }
+}
