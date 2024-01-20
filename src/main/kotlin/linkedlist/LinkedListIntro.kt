d852e0c8c08a0c948e07318658ddd8f1c0bb5de5
karkiayush <karkiayush619@gmail.com>
2024-01-20 19:22:38 +0545

    stack is started

---
+++ src/main/kotlin/linkedlist/LinkedListIntro.kt
@@ -0,0 +1,10 @@
+package linkedlist
+
+/**
+ * Elementary data structure is a fundamental building block used to organize and store data in the computer's memory.
+ * Some of the elementary data structure is:
+ * 1. Arrays
+ * 2. Linked List
+ * 3. Stack
+ * 4. Queue etc
+ * */
---
+++ src/main/kotlin/stack/StackIntro.kt
@@ -0,0 +1,6 @@
+package stack
+
+/**
+ * Stack is a Last-In-First-Out (LIFO) data structure. Elements are added and removed from the same end, known as the
+ * top. Common operations include push (add an element to the top) and pop (remove the top element).
+ * */
